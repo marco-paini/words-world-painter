638cd6cf5057b7401b4fd237e074b9b138ea63c5
marco_paini <m.paini@reply.it>
2016-10-24 18:47:51 +0200

    Initial commit

---
+++ Source/Sphinx_UE4_Test/Sphinx_UE4_TestGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "Sphinx_UE4_TestGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SPHINX_UE4_TEST_API ASphinx_UE4_TestGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
